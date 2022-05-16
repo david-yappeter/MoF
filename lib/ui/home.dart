163e7132b2b4f1c88445eb6e0190b918e057fc77
David Yappeter <davidyap11les@gmail.com>
2022-05-16 22:43:54 +0700

    adding test lib introduction_screen

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mof/controllers/transaction.dart';
+import 'package:mof/models/wallet.dart';
 import 'package:mof/router/custom_bottom_navigation.dart';
 import 'package:mof/ui/new_transaction.dart';
+import 'package:mof/ui/wallet.dart';
 import 'package:mof/widgets/custom_bottom_navigation_bar.dart';
 import 'package:mof/const/bottom_navigation.dart' as constant;
-import 'package:mof/widgets/custom_app_bar.dart';
+import 'package:mof/widgets/custom_tab_bar.dart';
 
 class Home extends GetView<BottomNavigationRouter> {
   static const routeName = '/';
@@ -22,10 +24,19 @@ class Home extends GetView<BottomNavigationRouter> {
         title: SizedBox(
           width: 200,
           child: ListTile(
-            leading: const Icon(Icons.account_circle),
+            leading: const Icon(Icons.account_circle, size: 40.0),
+            trailing: const Icon(Icons.arrow_drop_down, size: 30.0),
             title: Text('asd ${controller.currentIdx.value}'),
             subtitle: const Text('edgagr'),
-            onTap: () {},
+            onTap: () async {
+              final data =
+                  await Get.toNamed(WalletUI.routeName) as WalletModel?;
+              if (data == null) return;
+              print(data.id);
+              print(data.amount);
+              print(data.iconId);
+              print(data.name);
+            },
           ),
         ),
         bottom: CustomTabBar(),
